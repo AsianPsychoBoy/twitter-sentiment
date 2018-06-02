9f6e394ebdb97dd520092424b1a66fff7e43eb15
AsianPsychoBoy <ziyang_99@outlook.com>
2018-06-01 22:28:36 -0500

    cannot find __decorate

---
+++ twitter-sentiment/client/imports/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+ 
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MyApp {}
