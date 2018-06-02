7fdb4de77a527c666446e1ae5615b3d6ae7ee0b7
AsianPsychoBoy <ziyang_99@outlook.com>
2018-06-01 21:24:25 -0500

    god gammit why wouldnt' this compile

---
+++ twitter-sentiment/client/imports/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { MyApp } from './app.component';
+ 
+@NgModule({
+	declarations: [
+		MyApp
+	],
+	entryComponents: [
+		MyApp
+	],
+	providers: [
+	]
+})
+export class AppModule {}
