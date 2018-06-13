05ba74c7dcf4235b1d5206fef346dbee8e8c7129
AsianPsychoBoy <ziyang_99@outlook.com>
2018-06-13 14:03:57 -0500

    set up config.json

--- twitter-sentiment/server/main.ts
+++ twitter-sentiment/server/main.ts
@@ -2,16 +2,16 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
 	// code to run on server at startup
-	// ServiceConfiguration.configurations.upsert(
-	// 	{ service: 'twitter' },
-	// 	{
-	// 		$set: {
-	// 		loginStyle: "popup",
-	// 		consumerkey: "1292962797", // See table below for correct property name!
-	// 		secret: "75a730b58f5691de5522789070c319bc"
-	// 		}
-	// 	}
-	// );
+	ServiceConfiguration.configurations.upsert(
+		{ service: 'twitter' },
+		{
+			$set: {
+			loginStyle: 'popup',
+			consumerkey: Meteor.settings.twitter.consumerKey, // See table below for correct property name!
+			secret: Meteor.settings.twitter.consumerSecret
+			}
+		}
+	);
 	
 	
 	Meteor.methods({
