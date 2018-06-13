596ada822a1066ac450cba346df2d2d816a0aca5
AsianPsychoBoy <ziyang_99@outlook.com>
2018-06-13 13:32:40 -0500

    add config

--- twitter-sentiment/server/main.ts
+++ twitter-sentiment/server/main.ts
@@ -2,6 +2,18 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
 	// code to run on server at startup
+	// ServiceConfiguration.configurations.upsert(
+	// 	{ service: 'twitter' },
+	// 	{
+	// 		$set: {
+	// 		loginStyle: "popup",
+	// 		consumerkey: "1292962797", // See table below for correct property name!
+	// 		secret: "75a730b58f5691de5522789070c319bc"
+	// 		}
+	// 	}
+	// );
+	
+	
 	Meteor.methods({
 		authenticateTwitter() {
 			// Meteor.loginWithTwitter()
